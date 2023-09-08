e3925db0caf66e0c7fe2afc64b0957a51d6e0655
quantxz <indiedev.core@gmail.com>
2023-09-08 14:53:54 -0300

    dist update

---
+++ dist/src/index.d.ts
@@ -0,0 +1,3 @@
+import { Route } from "./Route";
+import { ServerStarter } from "./serverStarter";
+export { Route, ServerStarter };
---
+++ dist/src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ServerStarter = exports.Route = void 0;
+const Route_1 = require("./Route");
+Object.defineProperty(exports, "Route", { enumerable: true, get: function () { return Route_1.Route; } });
+const serverStarter_1 = require("./serverStarter");
+Object.defineProperty(exports, "ServerStarter", { enumerable: true, get: function () { return serverStarter_1.ServerStarter; } });
+//# sourceMappingURL=index.js.map
