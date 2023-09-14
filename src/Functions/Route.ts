b2e7032407faa54e7895dfd83595fffeed86bfba
ANDERSON LIMA DA SILVA <00001102357686sp@aluno.educacao.sp.gov.br>
2023-09-14 15:45:49 -0300

    update

--- src/Functions/Route.ts
+++ src/Functions/Route.ts
@@ -22,7 +22,7 @@ export const Route = (path: string, method: string, middlaware?: (...params: Exc
         try {
           await originalMethod.call(req, res, next);
 
-          /* como o original method não esta passando qual a instancia (Por exemplo Controller.index) o codigo é preciso passar no metodo; 
+          /* como o originalMethod não esta passando qual a instancia (Por exemplo Controller.index) o codigo é preciso passar no metodo; 
           na real isso esta passando agora eu tenho que criar uma route para colocar no App principal para que ele indetifique o metodo;
           um dos dois*/ 
         } catch (error) {
